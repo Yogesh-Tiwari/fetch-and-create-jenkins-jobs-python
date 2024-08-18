f2c2a00611cd164b970b83e52d3a2123e3d7d047
Yogesh <21yogeshtiwari@gmail.com>
2024-08-18 14:04:26 +0530

    base commit

---
+++ README.md
@@ -0,0 +1,4 @@
+To install the jenkins wrapper python module, use the command:
+
+pip install python-jenkins
+
---
+++ create-job.py
@@ -0,0 +1,24 @@
+import jenkins
+import json
+
+
+with open('secrets.json','r') as file:
+    config = json.load(file)
+
+server = jenkins.Jenkins('http://localhost:8080',username=config.get('username'),password=config.get('password'))
+
+jobs=server.get_jobs()
+
+for job in jobs:
+    if(job["_class"]=='com.cloudbees.hudson.plugins.folder.Folder'):
+
+        for childJob in job['jobs']:
+            with open("{}.xml".format(childJob['name']),'w') as file:
+                file.write(server.get_job_config("{}/{}".format(job['name'],childJob['name'])))
+    else:
+        with open(job['name'],'w') as file:
+            file.write(server.get_job_config(job['name']))
+
+
+
+# print(server.get_job_config('parent/multibranch-pipeline-child'))
