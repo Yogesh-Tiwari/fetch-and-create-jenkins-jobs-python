df160b1a40e30630e40c260f8ce942530f680dfb
Yogesh <21yogeshtiwari@gmail.com>
2024-08-18 14:10:36 +0530

    base commit 2

--- create-job.py
+++ create-job.py
@@ -11,14 +11,9 @@ jobs=server.get_jobs()
 
 for job in jobs:
     if(job["_class"]=='com.cloudbees.hudson.plugins.folder.Folder'):
-
         for childJob in job['jobs']:
             with open("{}.xml".format(childJob['name']),'w') as file:
                 file.write(server.get_job_config("{}/{}".format(job['name'],childJob['name'])))
     else:
         with open("{}.xml".format(job['name']),'w') as file:
             file.write(server.get_job_config(job['name']))
-
-
-
-# print(server.get_job_config('parent/multibranch-pipeline-child'))
