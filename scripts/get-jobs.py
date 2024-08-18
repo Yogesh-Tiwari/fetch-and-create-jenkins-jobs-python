4fa7f3473861be65a081dcef177f59d8d5973c24
Yogesh <21yogeshtiwari@gmail.com>
2024-08-18 18:33:50 +0530

    get-jobs. py working fine with all folder/subfolder creation

--- scripts/get-jobs.py
+++ scripts/get-jobs.py
@@ -7,22 +7,44 @@ with open('secrets/secret.json','r') as file:
 
 server = jenkins.Jenkins('http://localhost:8080',username=config.get('username'),password=config.get('password'))
 
+parentFolderName = 'testing'
+
 jobs=server.get_jobs()
 print(jobs)
-os.makedirs('configs',exist_ok=True)
+os.makedirs(parentFolderName,exist_ok=True)
+
+
+def createJobsInFolder(parentFolderPath,jobs):
+    for item in jobs:
+        if(item["_class"]=='com.cloudbees.hudson.plugins.folder.Folder'):
+            newPath = "{}/{}".format(parentFolderPath,item["name"])
+            os.makedirs(newPath,exist_ok=True)
+
+            with open("{}/{}.xml".format(newPath,item['name']),'w') as file:
+                file.write(server.get_job_config(job['name']))
+
+            createJobsInFolder(newPath,item['jobs'])
+        else:
+            with open("{}/{}.xml".format(parentFolderPath,item['name']),'w') as file:
+                temp = parentFolderPath.replace("{}/".format(parentFolderName),"")
+                file.write(server.get_job_config("{}/{}".format(temp,item['name'])))
+
 
 for job in jobs:
     if(job["_class"]=='com.cloudbees.hudson.plugins.folder.Folder'):
-        os.makedirs("configs/{}".format(job['name']),exist_ok=True)
-
-        with open("configs/{}/{}.xml".format(job['name'],job['name']),'w') as file:
-            file.write(server.get_job_config(job['name']))
+        newPath = "{}/{}".format(parentFolderName,job['name'])
         
-        for childJob in job['jobs']:
-            with open("configs/{}/{}.xml".format(job['name'],childJob['name']),'w') as file:
-                file.write(server.get_job_config("{}/{}".format(job['name'],childJob['name'])))
-    
-    else:
-        with open("configs/{}.xml".format(job['name']),'w') as file:
+        # create new folder for the child jobs
+        os.makedirs(newPath,exist_ok=True)
+
+        # create folder .xml file
+        with open("{}/{}.xml".format(newPath,job['name']),'w') as file:
             file.write(server.get_job_config(job['name']))
+
+        createJobsInFolder(newPath,job['jobs'])
+    else:
+        with open("{}/{}.xml".format(parentFolderName,job['name']),'w') as file:
+            file.write(server.get_job_config(job['name']))
+
+
 
