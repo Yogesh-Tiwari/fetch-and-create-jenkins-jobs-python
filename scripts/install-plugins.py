5b042672d24f2de8df6437e25d266941ccad16c4
Yogesh <21yogeshtiwari@gmail.com>
2024-10-02 21:24:32 +0530

    updated install plugins error resolution

--- scripts/install-plugins.py
+++ scripts/install-plugins.py
@@ -5,8 +5,8 @@ def get_plugins(server: jenkins.Jenkins) -> List[Dict]:
 
 def install_plugins(server: jenkins.Jenkins, plugins: List) -> None:
     for plugin in plugins:
-        # https://review.opendev.org/c/jjb/python-jenkins/+/719059 (install plugins error resolved PR link).
-        # To find the installed plugin path, we need to use the following command: pip show python-jenkins.
+        # (python-jenkins version 1.8.2) Edit the files specified in the PR to resolve the issue with installing plugins: https://review.opendev.org/c/jjb/python-jenkins/+/719059.
+        # To find the installed plugin path, we need to use the following command: "pip show python-jenkins" and refer to Location in the output.
         server.install_plugin(plugin, include_dependencies=True)
 
 def main():
